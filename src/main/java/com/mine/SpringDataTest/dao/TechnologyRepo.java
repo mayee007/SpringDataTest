302b24c09d2f1ba12257a1b1d2d494223fb8ec72
mayee007 <yelekeri@gmail.com>
2020-05-23 16:40:58 -0400

    added technology

---
+++ src/main/java/com/mine/SpringDataTest/dao/TechnologyRepo.java
@@ -0,0 +1,11 @@
+package com.mine.SpringDataTest.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.mine.SpringDataTest.Model.FullName;
+import com.mine.SpringDataTest.Model.Technology;
+
+@RepositoryRestResource(collectionResourceRel="technology", path="technology")
+public interface TechnologyRepo extends JpaRepository<Technology, Integer> {
+}
